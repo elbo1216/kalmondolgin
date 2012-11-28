a258f917356ad7ee69fc2e80688a2512d02516bd
Elton Chung <elbo1216@gmail.com>
2012-11-27 21:36:07 -0500

    Initial commit.  Blank Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kalmondolgin::Application.config.secret_token = '305a42edc01ff8a4e60e5ba0e80aad22d495e90aacb5c0a7b62e5746e740691307a4ae6e02f9fc0eec83b855dfb7fbc9d5cefeaac0c1a8e226b79b6c58f96658'
